c57d44b1f57bf323ed3e7b30f77a271e2baed370
wanderer1230 <49447626+wanderer1230@users.noreply.github.com>
2023-04-01 17:33:30 +0800

    Delete Duke_test.sh

--- scripts/Duke_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/scripts
-
-python Test.py  --arch 'PSTA'\
-                --dataset 'duke'\
-                --test_sampler 'dense'\
-                --triplet_distance 'cosine'\
-                --test_distance 'cosine'\

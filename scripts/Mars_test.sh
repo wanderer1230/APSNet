1ea7aa981ce3f2d8609a8c1baa8f18e8f9a6df68
wanderer1230 <49447626+wanderer1230@users.noreply.github.com>
2023-04-01 17:57:34 +0800

    Update Mars_test.sh

--- scripts/Mars_test.sh
+++ scripts/Mars_test.sh
@@ -5,4 +5,4 @@ python Test.py  --arch 'PSTA'\
                 --test_sampler 'Begin_interval'\
                 --triplet_distance 'cosine'\
                 --test_distance 'cosine'\
-                --test_path '/home/wyq/exp/ablation experiment/2021-01-15_22-38-01/rank1_0.91515154_checkpoint_ep350.pth'
+                --test_path ''

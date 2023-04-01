e547b63c8b09872709a050b51c96350679d5f78d
wanderer1230 <49447626+wanderer1230@users.noreply.github.com>
2023-04-01 17:33:36 +0800

    Delete Duke_train.sh

--- scripts/Duke_train.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/scripts
-
-python Train.py     --arch 'PSTA'\
-                    --dataset 'duke'\
-                    --test_sampler 'dense'\
-                    --triplet_distance 'cosine'\
-                    --test_distance 'cosine'\
